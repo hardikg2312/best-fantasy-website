c0cf2d9c4c09a9ee4f768b5c0d40f46c1ce6dea5
Hardik <hardikg23@gmail.com>
2017-05-03 02:11:48 +0530

    Adding website link

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,13 +5,13 @@ class Vote < ActiveRecord::Base
 
   # PREMIUM_FEATURE will be open if level is greater.
   FANTASY_OPTIONS = [
-      {:id => 1, :name => 'Fanspole', :logo => 'site-logo/fanspole.png', :path => 'fanspole'}, 
-      {:id => 2, :name => 'Dream11', :logo => 'site-logo/dream11.png', :path => 'dream11'}, 
-      {:id => 3, :name => 'OyeCaptain', :logo => 'site-logo/oyecaptain.png', :path => 'oyecaptain'},
-      {:id => 4, :name => 'Fandromeda', :logo => 'site-logo/fandromeda.png', :path => 'fandromeda'},
-      {:id => 5, :name => 'CricBattle', :logo => 'site-logo/cricbattle.png', :path => 'cricbattle'}, 
-      {:id => 6, :name => 'Cricketinc', :logo => 'site-logo/cricketinc.png', :path => 'cricketinc'}, 
-      {:id => 7, :name => 'DailyFantasy Cricket', :logo => 'site-logo/daily-fantasy-cricket.png', :path => 'daily-fantasy-cricket'}
+      {:id => 1, :name => 'Fanspole', :logo => 'site-logo/fanspole.png', :path => 'fanspole', :site => 'https://www.fanspole.com/'}, 
+      {:id => 2, :name => 'Dream11', :logo => 'site-logo/dream11.png', :path => 'dream11', :site => 'https://fantasycricket.dream11.com'}, 
+      {:id => 3, :name => 'OyeCaptain', :logo => 'site-logo/oyecaptain.png', :path => 'oyecaptain', :site => 'https://www.oyecaptain.com/'},
+      {:id => 4, :name => 'Fandromeda', :logo => 'site-logo/fandromeda.png', :path => 'fandromeda', :site => 'https://fandromeda.com/event'},
+      {:id => 5, :name => 'CricBattle', :logo => 'site-logo/cricbattle.png', :path => 'cricbattle', :site => 'http://www.cricbattle.com/'}, 
+      {:id => 6, :name => 'Cricketinc', :logo => 'site-logo/cricketinc.png', :path => 'cricketinc', :site => 'https://www.cricketinc.com/'}, 
+      {:id => 7, :name => 'DailyFantasy Cricket', :logo => 'site-logo/daily-fantasy-cricket.png', :path => 'daily-fantasy-cricket', :site => 'https://www.dailyfantasycricket.com'}
     ]
 
 end
